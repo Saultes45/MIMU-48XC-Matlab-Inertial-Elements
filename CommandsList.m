641fd67569d4cafc32d5f174d0a672d4fcc70ab0
Nathan <nathanael.esnault@gmail.com>
2021-07-09 16:20:29 +1200

    Update CommandsList.m

--- CommandsList.m
+++ CommandsList.m
@@ -17,10 +17,15 @@
 %% Possible Improvements
 
 %% TODO
-% list from 2 documents
-% MIMU4844_48XC-Integration-Guide.pdf
-% and
 
+%% Ressources
+% list from 2 documents:
+% MIMU4844_48XC-Integration-Guide.pdf (D:\Dropbox\51_MIMU Inertial Elements\00_Documents\Integration Guide)
+% and
+% MIMU48XC-microSDCard-Integration-Guide.pdf (D:\Dropbox\51_MIMU Inertial Elements\00_Documents\Integration Guide MIMU48XC+microSD)
+
+% I created a spreadsheet to regroup
+% Google Sheets: university account: MIMU48XC command table
 
 %% Commands
 % Do NOT edit the next mimuCommad lines
@@ -37,6 +42,33 @@ mimuCommandsHeader.abc_1        = 76;
 mimuCommandsHeader.abc_1        = 77;
 mimuCommandsHeader.abc_1        = 78;
 mimuCommandsHeader.abc_1        = 79;
+
+mimuCommandsHeader.abc_1        = 80;
+mimuCommandsHeader.abc_1        = 77;
+mimuCommandsHeader.abc_1        = 78;
+mimuCommandsHeader.abc_1        = 79;
+
+mimuCommandsHeader.abc_1        = 80;
+mimuCommandsHeader.abc_1        = 81;
+mimuCommandsHeader.abc_1        = 82;
+mimuCommandsHeader.abc_1        = 83;
+mimuCommandsHeader.abc_1        = 84;
+mimuCommandsHeader.abc_1        = 85;
+mimuCommandsHeader.abc_1        = 86;
+
+mimuCommandsHeader.abc_1        = 88;
+mimuCommandsHeader.abc_1        = 89;
+mimuCommandsHeader.abc_1        = 90;
+mimuCommandsHeader.abc_1        = 91;
+mimuCommandsHeader.abc_1        = 92;
+mimuCommandsHeader.abc_1        = 93;
+
+mimuCommandsHeader.cmd_ack        = 93;
+
+% There are 2 kinds of acknowledgement:
+
+%Data aknowledge 		"PKG ACK" 		5 bytes <--- The F? what is this documentation?
+%Command aknowledge 	"    ACK" 		4bytes  <--- The F? what is this documentation?
 
 
 % END OF SCRIPT
