9b93f90c01fb9c3c6d0e24640935987c3292756f
Nathan <nathanael.esnault@gmail.com>
2021-08-28 09:56:52 +1200

    Update A_MenuMIMU.m

--- A_MenuMIMU.m
+++ A_MenuMIMU.m
@@ -70,34 +70,37 @@ end
 %% Menu: Asking User to chose data aquisition mode
 
 if occured_error == 0
-    Data_Processing_Mode = menu ('Chose data aquisition mode','Real Time', 'Log on SD card', 'Retrieve from SD card','Cancel');
-    % Real Time/Replay(Matlab Aquisition Files)/SD card(from boat)/GPS only
-    % RMC/RT long aquisition/Delta t find/Cancel
+    
+    Data_Processing_Mode = menu ( ...
+    'Chose data aquisition mode',...
+    'Real Time', 'Log on SD card',...
+    'Retrieve from SD card',...
+    'Cancel'...
+    );
     
     if  Data_Processing_Mode ~= 0
         switch Data_Processing_Mode
+            
             case 1 %% Real Time
                 Message(1,Do.MessageLog,0,'Data_Processing_Mode : Real Time', 'UDEF', RunID);
                 
-                %                 Bathymetry_RT_Mode;
+                % Bathymetry_RT_Mode;
                 AskRawOrPrecision;
                 RealTimeACQ;
                 
             case 2 %% Replay
                 Message(1,Do.MessageLog,0,'Data_Processing_Mode : Log on SD card', 'UDEF', RunID);
                 
-                %                 Bathymetry_Replay_Mode;
+                % Bathymetry_Replay_Mode;
                 
             case 3 %% SD card
                 Message(1,Do.MessageLog,0,'Data_Processing_Mode : Retrieve from SD card', 'UDEF', RunID);
                 
-                %                 Bathmetry_SD_Card;
+                % Bathmetry_SD_Card;
                 
             case 7 %% Cancel
                 Message(1,Do.MessageLog,0,'User asked to cancel' , 'UDEF', RunID);
                 occured_error = 1;
-                %The previous statement avoid an error if
-                %(DisplayFinalGraph == 1 AND the user cancelled the menu SO the varaibles to display wouldn't exist)
                 
         end% end for the switch on data aquisition method
     else
@@ -111,4 +114,5 @@ end % initial occured_error check;
 
 
 Message(1,Do.MessageLog,0,'End of the main Matlab script' , 'UDEF', RunID);
+
 %END OF SCRIPT
