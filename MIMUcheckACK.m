084a8423b9ee20421e901efcd38047d8ad600221
Nathan <nathanael.esnault@gmail.com>
2021-08-28 09:56:43 +1200

    Create MIMUcheckACK.m

---
+++ MIMUcheckACK.m
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Claculate what the ACK message from the MIMU should be
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Metadata
+% Written by    : Nathanaël Esnault
+% Verified by   : N/A
+% Creation date : 2021-08-27
+% Version       : 0.1 (finished on ...)
+% Modifications :
+% Known bugs    :
+
+%% Functions associated with this code :
+
+%% Possible Improvements
+
+%% TODO
+
+
+%% Code
+
+function desired_answer = MIMUcheckACK(messageIn)
+
+
+if ~isempty(messageIn)
+    
+end
+
+
+
+end
